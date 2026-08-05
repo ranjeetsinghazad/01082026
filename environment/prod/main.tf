a912ddf6c035943c8e4e4dd4aa8de8c57d3e9310
Ranjeet Singh <ranjeetsinghazad11@gmail.com>
2026-08-05 16:36:27 +0530

    Update main.tf

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -1,13 +1,1 @@
-title = "Custom Gitleaks config"
 
-[[rules]]
-id = "terraform-password"
-description = "Detect passwords"
-regex = '''(?i)(password|passwd|pwd)\s*[:=]\s*["']?[^"'\s]+'''
-tags = ["password"]
-
-[[rules]]
-id = "terraform-username"
-description = "Detect usernames"
-regex = '''(?i)(username|user_name|admin_username)\s*[:=]\s*["']?[^"'\s]+'''
-tags = ["username"]
